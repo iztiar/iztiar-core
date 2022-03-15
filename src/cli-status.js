73504e0568894ff93216757beef7891d41427a1c
iztiar-0 <iztiar-leader@trychlos.org>
2022-03-15 16:22:49 +0100

    cliStatus() updated accordingly

--- src/cli-status.js
+++ src/cli-status.js
@@ -1,21 +1,22 @@
 /*
  * cli-status.js
  * 
- *  Returns a Promise which eventually resolves with the service status.
+ *  Returns a Promise which eventually resolves with the feature status.
  */
 import chalk from 'chalk';
 
-import { IServiceable, Msg } from './index.js';
+import { IServiceable, featureCard, Msg } from './index.js';
 
 /**
  * Get the status of the named service
  * @param {coreApi} api a coreApi instance
- * @param {String} name the service name to be started
+ * @param {String} name the feature name to get the status from
  * @param {Object} options 
  *  consoleLevel {String} defaulting to current level
  * @returns {Promise} which resolves to the service status
  */
 export function cliStatus( api, name, options={} ){
+    Msg.verbose( 'cliStatus()', 'coreApi:', api, 'name='+name, 'options:', options );
 
     const _origLevel = Msg.consoleLevel();
     const _consoleLevel = Object.keys( options ).includes( 'consoleLevel' ) ? options.consoleLevel : _origLevel;
@@ -23,43 +24,47 @@ export function cliStatus( api, name, options={} ){
 
     Msg.out( 'Getting the status of \''+name+'\' service' );
 
-    const service = api.pluginManager().byName( api, name );
-    let _promise = Promise.resolve( true );
+    const feature = api.pluginManager().byName( api, name );
+
+    if( !feature || !( feature instanceof featureCard )){
+        Msg.error( 'cliStatus() unknown feature: \''+name+'\'' );
+        process.exitCode += 1;
+        return Promise.resolve( false );
+    }
+
     let result = {};
 
-    // service.Initialize() must resolve with a IServiceable instance
-    const _checkInitialize = function( res ){
-        return IServiceable.isIServiceable( res )
-            .then(( success ) => { result.iServiceable = success ?  { ...res } : null; });
-    };
+    let _promise = Promise.resolve( true )
+        .then(() => { return feature.initialize( api ); })
+        .then(( iServiceable ) => {
+            if( iServiceable && iServiceable instanceof IServiceable ){
+                Msg.verbose( name+': iServiceable sucessfully initialized' );
+            } else {
+                Msg.verbose( name+': initialization failed' );
+            }
+        })
+        .then(( res ) => { return feature.status(); })
+        .then(( res ) => {
 
-    if( service ){
-        _promise = _promise
-            .then(() => { return service.initialize( api ); })
-            .then(( res ) => { return _checkInitialize( res ); })
-            .then(( res ) => { return service.status(); })
-            .then(( res ) => {
+            if( res.startable ){
+                Msg.out( 'Service doesn\'t run, is startable' );
+            
+            } else if( res.reasons.length  ){
+                Msg.warn( '\''+name+'\' service is said running, but exhibits', res.reasons.length, 'error message(s)' );
+                Msg.warn( ' You may want use --force-stop option to remove the falsy \''+name+'\' from your run directory' );
+                process.exitCode += 1;
 
-                if( res.startable ){
-                    Msg.out( 'Service doesn\'t run, is startable' );
-                
-                } else if( res.reasons.length  ){
-                    Msg.warn( '\''+name+'\' service is said running, but exhibits', res.reasons.length, 'error message(s)' );
-                    Msg.warn( ' You may want use --force-stop option to remove the falsy \''+name+'\' from your run directory' );
-                    process.exitCode += 1;
-
-                } else {
-                    Msg.out( chalk.green( 'Service \''+name+'\' is confirmed up and running' ));
-                    const hello = result.iServiceable.helloMessage();
-                    if( hello ){
-                        Msg.out( chalk.green( 'Greetings message is « '+hello+' »' ));
-                    }
+            } else {
+                Msg.out( chalk.green( 'Service \''+name+'\' is confirmed up and running' ));
+                const hello = feature.iServiceable().get( 'helloMessage' );
+                if( hello ){
+                    Msg.out( chalk.green( 'Greetings message is « '+hello+' »' ));
                 }
+            }
 
-                if( _consoleLevel !== _origLevel ) Msg.consoleLevel( _origLevel );
-                return Promise.resolve( res );
-            })
-    }
+            if( _consoleLevel !== _origLevel ) Msg.consoleLevel( _origLevel );
+            return Promise.resolve( res );
+        })
 
     return _promise;
 }
